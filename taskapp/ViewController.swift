4c881052b66ca947a16e569bdef352ca66664ee8
白鳥雅人 <whitesplash0703@icloud.com>
2019-09-26 06:26:27 +0900

    pod installと.gitinoreの作成まで実施

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    // MARK: UITableViewDataSourceプロトコルのメソッド
+    // データの数（＝セルの数）を返すメソッド
+    func tablView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    // 各セルの内容を返すメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能なcellを得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    //MARK: UITableViewのプロトコルのメソッド
+    //各セルを選択したときに実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue", sender: nil) // ←追加する
+    }
+    // セルが削除可能なことを伝えるメソッド
+    func tableView(_ tabvleView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    }
 }
 
