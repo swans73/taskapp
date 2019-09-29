f60e35770f0677baee66d2a22ceb18fe9a15b527
白鳥雅人 <whitesplash0703@icloud.com>
2019-09-29 15:58:44 +0900

    lesson6 6.10まで

--- taskapp/Task.swift
+++ taskapp/Task.swift
@@ -14,6 +14,8 @@ class Task: Object {
     //タイトル
     @objc dynamic var title = ""
     //内容
+    @objc dynamic var contents = ""
+    //日時
     @objc dynamic var date = Date()
     /** idをプライマリーキーとして設定 **/
     override static func primaryKey() -> String? {
